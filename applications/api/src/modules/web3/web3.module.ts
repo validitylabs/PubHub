56490ab87145dcd29ba714164cb76d4d775a7cf1
ChenchenYo <yuqianchen93@gmail.com>
2019-05-10 15:53:25 +0200

    ✨  backend extracts the latest ipfs digest from the smart contract

--- applications/api/src/modules/web3/web3.module.ts
+++ applications/api/src/modules/web3/web3.module.ts
@@ -1,7 +1,9 @@
 import {Module} from '@nestjs/common';
 import {Web3Service} from './web3.service';
+import {Web3Controller} from './web3.controller';
 
 @Module({
+    controllers: [Web3Controller],
     providers: [
         {
             provide: Web3Service,
--- applications/api/src/modules/web3/web3.service.ts
+++ applications/api/src/modules/web3/web3.service.ts
@@ -1,16 +1,99 @@
-import {Logger} from '@nestjs/common';
-import Web3 from 'web3';
+import {Logger, Injectable} from '@nestjs/common';
+// import Web3 from 'web3';
+// tslint:disable-next-line: no-require-imports
+import Web3 = require('web3');
 
+@Injectable()
 export class Web3Service {
     private readonly logger = new Logger('WEB3');
     private readonly web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/b663cde1acc040abbbd59c5dbb7de904'));
+    private readonly contract = new this.web3.eth.Contract(
+        [
+            {
+                constant: false,
+                inputs: [
+                    {
+                        name: 'digest',
+                        type: 'string'
+                    }
+                ],
+                name: 'writeToIpfs',
+                outputs: [],
+                payable: false,
+                stateMutability: 'nonpayable',
+                type: 'function'
+            },
+            {
+                anonymous: false,
+                inputs: [
+                    {
+                        indexed: true,
+                        name: 'Account',
+                        type: 'address'
+                    },
+                    {
+                        indexed: false,
+                        name: 'IpfsDigest',
+                        type: 'string'
+                    }
+                ],
+                name: 'AddedToIpfs',
+                type: 'event'
+            },
+            {
+                constant: true,
+                inputs: [],
+                name: 'counter',
+                outputs: [
+                    {
+                        name: '',
+                        type: 'uint256'
+                    }
+                ],
+                payable: false,
+                stateMutability: 'view',
+                type: 'function'
+            }
+        ],
+        '0x3db7f3ce55f73ba200e2ad9e96c81627448255c2'
+    );
 
     constructor() {
         this.logger.log(` web3 module is logging`);
-        this.web3.eth.net.getNetworkType().then((network) => {
-            this.logger.log(` web3 we are on ${network} network.`);
-        });
+        this.web3.eth.net
+            .getNetworkType()
+            .catch((error) => this.logger.log(` [web3 module] has an error ${error}`))
+            .then((network) => {
+                this.logger.log(` [web3 module] we are on ${network} network.`);
+            })
+            .catch(() => '[web3 module] obligatory catch');
+        this.logger.log(` [web3 module] initializes contract at ${this.contract.address}`);
     }
+
+    async getExistingEventLength(): Promise<number> {
+        // this.contract
+        //     .getPastEvents('allEvents', {fromBlock: 0, toBlock: 'latest'})
+        //     .catch((error) => this.logger.log(` [web3 module pastEvents] has an error ${error}`))
+        //     .then((events) => {
+        //         this.logger.log(` [web3 module pastEvents] got events ${events}`);
+        //     })
+        //     .catch(() => '[web3 module] obligatory catch');
+        const events = await this.contract.getPastEvents('allEvents', {fromBlock: 0, toBlock: 'latest'});
+        return events.length;
+    }
+
+    async getLastestEventHash(): Promise<string> {
+        const events = await this.contract.getPastEvents('allEvents', {fromBlock: 0, toBlock: 'latest'});
+        const IpfsDigestString = events[events.length - 1].returnValues.IpfsDigest.slice(1, -1);
+        if (IpfsDigestString.startsWith('Qm')) {
+            return IpfsDigestString;
+            // tslint:disable-next-line: no-unnecessary-else
+            // tslint:disable-next-line: no-else-after-return
+        } else {
+            return 'Qm'.concat(IpfsDigestString);
+        }
+    }
+
     // extractValidationErrorMessages(validationErrors: ValidationError[]) {
     //     return validationErrors
     //         .map(
